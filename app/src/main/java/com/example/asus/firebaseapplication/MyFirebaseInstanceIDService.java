8b7423bbcbdabc12eec8dd69691840f41b8a25a0
devialrst <devialarasati@student.uns.ac.id>
2018-11-25 19:19:47 +0700

    first commit

---
+++ app/src/main/java/com/example/asus/firebaseapplication/MyFirebaseInstanceIDService.java
@@ -0,0 +1,13 @@
+package com.example.asus.firebaseapplication;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public void onTokenRefresh(){
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FirebaseToken", "Refreshed Token = " + refreshedToken);
+    }
+}
